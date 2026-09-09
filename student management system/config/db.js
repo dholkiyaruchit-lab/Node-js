e0bf28b826872cd59ca8f4ce4343db1490a00448
dholkiyaruchit-lab <dholkiyaruchit@gmail.com>
2026-09-09 08:44:54 +0530

    code added

---
+++ student management system/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+  try {
+    const connect = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/studentManagementSystem",
+    );
+
+    console.log("db connected");
+
+    return connect;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export default connectDB;
